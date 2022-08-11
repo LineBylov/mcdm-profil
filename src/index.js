4f43afd13b03b2fff9ba08fb11c7cbdabe15aac4
LineBylov <lovebylov@gmail.com>
2022-08-11 10:27:09 +0200

    o

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-console.log('bye bye bye baby bye bye bye byebye')
 
+const renderProfileData = () => {
 
-
-fetch("/data/profil.json")
-.then((respons) => respons.json())
-.then((data) => {
-
-  console.log(data)
-
-  let imageContainer = document.querySelector('.profil-image')
-  imageContainer.src = data.avatar_url;
-  let imageText = document.querySelector('.profil-name')
- // imageText.innerHTML = data.login;
-
- // imageText.innerHTML = `<b>${data.login}<b>`;
- // imageText.innerHTML = '<b>' + data.login + '<b>';
-  imageText.insertAdjacentHTML('beforeend', `<b>${data.login}<b>`)
-
-  console.log(imageContainer.src)
-});
-
-function open() {
-  console.log('open')
+  fetch("/data/profil.json")
+  .then((respons) => respons.json())
+  .then((data) => {
+  
+    console.log(data)
+  
+    let imageContainer = document.querySelector('.profil-image')
+    imageContainer.src = data.avatar_url;
+    let imageText = document.querySelector('.profil-name')
+   // imageText.innerHTML = data.login;
+  
+    imageText.innerHTML = `<b>${data.login}<b>`;
+   // imageText.innerHTML = '<b>' + data.login + '<b>';
+   // imageText.insertAdjacentHTML('beforeend', `<b>${data.login}<b>`)
+  
+   // console.log(imageContainer.src)
+  });
 }
-
-open();
+renderProfileData();
